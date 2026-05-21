19c9c37a3e2fe76067e88bbeec72ea939f55fc30
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-05-21 16:19:27 -0300

    refactor: keep edit match route thin

--- web/src/app/groups/[groupId]/matches/[matchId]/edit/page.tsx
+++ web/src/app/groups/[groupId]/matches/[matchId]/edit/page.tsx
@@ -1,10 +1,5 @@
-import { notFound } from 'next/navigation';
-import { AddMatchForm } from '@/features/matches/components/add-match-form';
 import { AppShell } from '@/components/app-shell';
-import { BackButton } from '@/components/back-button';
-import { PageHeader } from '@/components/page-header';
-import { getGroup, getGroupMembers } from '@/features/groups/api/groups.api';
-import { getGroupMatch } from '@/features/matches/api/matches.api';
+import { EditGroupMatch } from '@/features/matches/edit-group-match';
 
 type Props = {
   params: Promise<{
@@ -16,23 +11,9 @@ type Props = {
 export default async function EditGroupMatchPage({ params }: Props) {
   const { groupId, matchId } = await params;
 
-  try {
-    const [group, members, match] = await Promise.all([
-      getGroup(groupId),
-      getGroupMembers(groupId),
-      getGroupMatch(groupId, matchId),
-    ]);
-
-    return (
-      <AppShell>
-        <div className="space-y-6">
-          <BackButton href={['', 'groups', group.id].join('/')} />
-          <PageHeader title="Corrigir partida" description={group.name} />
-          <AddMatchForm groupId={group.id} members={members} match={match} />
-        </div>
-      </AppShell>
-    );
-  } catch {
-    notFound();
-  }
+  return (
+    <AppShell>
+      <EditGroupMatch groupId={groupId} matchId={matchId} />
+    </AppShell>
+  );
 }
