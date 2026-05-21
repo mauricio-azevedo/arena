fce2557a046a7c7938a44bf54c624b29a1b8ba70
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-05-21 13:48:31 -0300

    feat: add public user profiles

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -8,6 +8,7 @@ import { GroupInvitesModule } from './group-invites/group-invites.module';
 import { MeModule } from './me/me.module';
 import { MembersModule } from './members/members.module';
 import { FeedModule } from './feed/feed.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { FeedModule } from './feed/feed.module';
     GroupInvitesModule,
     MeModule,
     FeedModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
