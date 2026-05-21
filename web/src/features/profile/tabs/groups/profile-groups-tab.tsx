059d573444d3e79ceb45b46c456d1f790ca6930b
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-05-21 13:59:01 -0300

    feat: support public profile groups

--- web/src/features/profile/tabs/groups/profile-groups-tab.tsx
+++ web/src/features/profile/tabs/groups/profile-groups-tab.tsx
@@ -1,5 +1,14 @@
 import { MyGroupsList } from '@/features/groups/components/my-groups-list';
+import { getPublicProfileGroups } from '@/features/profile/profile-user.api';
 
-export function ProfileGroupsTab() {
+type Props = {
+  userId?: string;
+};
+
+export function ProfileGroupsTab({ userId }: Props) {
+  if (userId) {
+    return <MyGroupsList loadGroups={() => getPublicProfileGroups(userId)} ratingLabel="Rating" />;
+  }
+
   return <MyGroupsList />;
 }
