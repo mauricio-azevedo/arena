93eaccd4bbd60a905e21ce3178490388b12a51f4
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-05-21 13:51:14 -0300

    feat: add public user profiles

---
+++ api/src/users/users.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
+import { ProfileMatchesService } from '../me/profile-matches/profile-matches.service';
+import { ProfileSummaryGroupsService } from '../me/profile-summary/profile-summary-groups.service';
+import { ProfileSummaryMatchesService } from '../me/profile-summary/profile-summary-matches.service';
+import { ProfileSummaryService } from '../me/profile-summary/profile-summary.service';
+import { ProfileSummaryStatsService } from '../me/profile-summary/profile-summary-stats.service';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [UsersController],
+  providers: [
+    UsersService,
+    ProfileSummaryService,
+    ProfileSummaryStatsService,
+    ProfileSummaryMatchesService,
+    ProfileSummaryGroupsService,
+    ProfileMatchesService,
+  ],
+})
+export class UsersModule {}
