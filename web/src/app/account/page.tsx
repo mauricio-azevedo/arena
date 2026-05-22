df021cd021eecb8ec2020f346bb565e0d11b9890
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-05-22 11:35:47 -0300

    feat: add account page route

---
+++ web/src/app/account/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from '@/components/app-shell';
+import { Account } from '@/features/account/account';
+
+export default function AccountPage() {
+  return (
+    <AppShell>
+      <Account />
+    </AppShell>
+  );
+}
