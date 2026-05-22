2a128363cb86c9606e94052e9f766abf0b0a12cf
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-05-22 17:01:39 -0300

    avoid incorrect back label while loading public profile

--- web/src/app/users/[userId]/loading.tsx
+++ web/src/app/users/[userId]/loading.tsx
@@ -1,14 +1,10 @@
 import { AppShell } from '@/components/app-shell';
-import { BackButton } from '@/components/back-button';
 import { ProfileLoadingState } from '@/features/profile/components/profile-loading-state';
 
 export default function UserProfileLoading() {
   return (
     <AppShell>
-      <div className="space-y-6">
-        <BackButton href="/groups" />
-        <ProfileLoadingState />
-      </div>
+      <ProfileLoadingState />
     </AppShell>
   );
 }
