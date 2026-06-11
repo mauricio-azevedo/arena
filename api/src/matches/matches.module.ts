2e71fbec5c05825ef20efc430bd5e51170088aa3
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-10 22:24:29 -0300

    Inject ranking movement service into matches

--- api/src/matches/matches.module.ts
+++ api/src/matches/matches.module.ts
@@ -4,9 +4,10 @@ import { MatchesController } from './matches.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
 import { FeedModule } from '../feed/feed.module';
+import { RankingModule } from '../ranking/ranking.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, FeedModule],
+  imports: [PrismaModule, AuthModule, FeedModule, RankingModule],
   controllers: [MatchesController],
   providers: [MatchesService],
 })
