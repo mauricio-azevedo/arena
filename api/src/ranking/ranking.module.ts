8b5ef267ca2da3d3945fc5d64573f4b53d75e80f
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-10 22:24:11 -0300

    Export ranking movement service

--- api/src/ranking/ranking.module.ts
+++ api/src/ranking/ranking.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RankingService } from './ranking.service';
 import { RankingController } from './ranking.controller';
+import { RankingMovementService } from './ranking-movement.service';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [RankingController],
-  providers: [RankingService],
+  providers: [RankingService, RankingMovementService],
+  exports: [RankingMovementService],
 })
 export class RankingModule {}
