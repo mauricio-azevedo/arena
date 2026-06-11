531924f2f2eeed63d11dbbcb29416c365ca25a4f
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-11 00:19:38 -0300

    Import auth module in processing module

--- api/src/processing/processing.module.ts
+++ api/src/processing/processing.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { FeedModule } from '../feed/feed.module';
 import { RankingModule } from '../ranking/ranking.module';
+import { AuthModule } from '../auth/auth.module';
 import { ProcessingController } from './processing.controller';
 import { ProcessingJobReaderService } from './processing-job-reader.service';
 import { ProcessingJobRunnerService } from './processing-job-runner.service';
@@ -9,7 +10,7 @@ import { ProcessingJobWriterService } from './processing-job-writer.service';
 import { ProcessingWorkerService } from './processing-worker.service';
 
 @Module({
-  imports: [PrismaModule, FeedModule, RankingModule],
+  imports: [PrismaModule, FeedModule, RankingModule, AuthModule],
   controllers: [ProcessingController],
   providers: [
     ProcessingJobReaderService,
