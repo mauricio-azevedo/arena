a75d8909fa3055a51db714c5d755907c9deba262
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-14 01:38:20 -0300

    Remove obsolete page header from edit match

--- web/src/features/matches/edit-group-match.tsx
+++ web/src/features/matches/edit-group-match.tsx
@@ -1,6 +1,4 @@
 import { notFound } from 'next/navigation';
-import { BackButton } from '@/components/back-button';
-import { PageHeader } from '@/components/page-header';
 import { getGroup, getGroupMembers } from '@/features/groups/api/groups.api';
 import { getGroupMatch } from './api/matches.api';
 import { AddMatchForm } from './components/add-match-form';
@@ -18,13 +16,7 @@ export async function EditGroupMatch({ groupId, matchId }: Props) {
       getGroupMatch(groupId, matchId),
     ]);
 
-    return (
-      <div className="space-y-6">
-        <BackButton href={['', 'groups', group.id].join('/')} />
-        <PageHeader title="Corrigir partida" description={group.name} />
-        <AddMatchForm groupId={group.id} members={members} match={match} />
-      </div>
-    );
+    return <AddMatchForm groupId={group.id} members={members} match={match} />;
   } catch {
     notFound();
   }
