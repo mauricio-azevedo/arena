059c6be8115c8ca5a8c5f9484320227ee147d15f
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-13 22:14:38 -0300

    Use group title as initial page chrome

--- web/src/app/groups/[groupId]/page.tsx
+++ web/src/app/groups/[groupId]/page.tsx
@@ -1,4 +1,6 @@
+import { notFound } from 'next/navigation';
 import { AppShell } from '@/components/app-shell';
+import { getGroup } from '@/features/groups/api/groups.api';
 import { GroupDetail } from '@/features/groups/group-detail';
 
 type Props = {
@@ -13,9 +15,10 @@ type Props = {
 export default async function GroupDetailPage({ params, searchParams }: Props) {
   const { groupId } = await params;
   const { tab } = await searchParams;
+  const group = await getGroup(groupId).catch(() => notFound());
 
   return (
-    <AppShell>
+    <AppShell chrome={{ title: group.name, showBack: true, backHref: '/', preferBackHref: true }}>
       <GroupDetail groupId={groupId} tab={tab} />
     </AppShell>
   );
