64f6ff122f35d06197616bca889bc400d226e22e
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2026-06-14 03:17:35 -0300

    Refactor: Improve `layout.tsx` readability by formatting class declarations and adding `dark` mode to `<body>`
    Signed-off-by: Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata, Viewport } from 'next';
 import { Geist, Geist_Mono, Inter } from 'next/font/google';
 import './globals.css';
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -48,8 +48,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}>
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html
+      lang="pt-BR"
+      className={cn(
+        'h-full',
+        'antialiased',
+        geistSans.variable,
+        geistMono.variable,
+        'font-sans',
+        inter.variable,
+      )}
+    >
+      <body className="min-h-full flex flex-col dark">{children}</body>
     </html>
   );
 }
