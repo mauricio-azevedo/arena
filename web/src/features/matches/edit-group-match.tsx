85de588cbeaaf1e8818837c9ba3dca11ec91c4ae
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-15 02:13:04 -0300

    Remove legacy back button from edit match

--- web/src/features/matches/edit-group-match.tsx
+++ web/src/features/matches/edit-group-match.tsx
@@ -1,6 +1,5 @@
 import { notFound } from 'next/navigation';
-import { BackButton } from '@/components/back-button';
-import { PageHeader } from '@/components/page-header';
+import { PageIntro } from '@/components/page-intro';
 import { getGroup, getGroupMembers } from '@/features/groups/api/groups.api';
 import { getGroupMatch } from './api/matches.api';
 import { AddMatchForm } from './components/add-match-form';
@@ -20,8 +19,7 @@ export async function EditGroupMatch({ groupId, matchId }: Props) {
 
     return (
       <div className="space-y-6">
-        <BackButton href={['', 'groups', group.id].join('/')} />
-        <PageHeader title="Corrigir partida" description={group.name} />
+        <PageIntro description={group.name} />
         <AddMatchForm groupId={group.id} members={members} match={match} />
       </div>
     );
