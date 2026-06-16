302baeb224fc85fe33815310658f2cdea8c60230
Maurício Azevedo <131198882+mauricio-azevedo@users.noreply.github.com>
2026-06-16 20:51:36 -0300

    Import platform trending module into processing

--- api/src/processing/processing.module.ts
+++ api/src/processing/processing.module.ts
@@ -4,6 +4,7 @@ import { FeedModule } from '../feed/feed.module';
 import { RankingModule } from '../ranking/ranking.module';
 import { AuthModule } from '../auth/auth.module';
 import { GroupsModule } from '../groups/groups.module';
+import { PlatformTrendingModule } from '../platform-trending/platform-trending.module';
 import { RatingProjectionService } from '../rating/rating-projection.service';
 import { ProcessingController } from './processing.controller';
 import { ProcessingJobReaderService } from './processing-job-reader.service';
@@ -12,7 +13,14 @@ import { ProcessingJobWriterService } from './processing-job-writer.service';
 import { ProcessingWorkerService } from './processing-worker.service';
 
 @Module({
-  imports: [PrismaModule, FeedModule, RankingModule, AuthModule, GroupsModule],
+  imports: [
+    PrismaModule,
+    FeedModule,
+    RankingModule,
+    AuthModule,
+    GroupsModule,
+    PlatformTrendingModule,
+  ],
   controllers: [ProcessingController],
   providers: [
     RatingProjectionService,
