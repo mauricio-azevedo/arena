0bdda70f394a7cddcdf677fb5928483a27d9ad9a
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2026-06-17 03:50:17 -0300

    Feat: Export `PlatformTrendingPlayersReadService` from `PlatformTrendingModule`
    Signed-off-by: Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>

--- api/src/platform-trending/platform-trending.module.ts
+++ api/src/platform-trending/platform-trending.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PlatformTrendingPlayersProjectionService } from './platform-trending-players-projection.service';
+import { PlatformTrendingPlayersReadService } from './platform-trending-players-read.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [PlatformTrendingPlayersProjectionService],
-  exports: [PlatformTrendingPlayersProjectionService],
+  providers: [
+    PlatformTrendingPlayersProjectionService,
+    PlatformTrendingPlayersReadService,
+  ],
+  exports: [
+    PlatformTrendingPlayersProjectionService,
+    PlatformTrendingPlayersReadService,
+  ],
 })
 export class PlatformTrendingModule {}
