ddea98bb5811d5dfed26b092cb1d94f87efde974
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2026-06-17 03:54:48 -0300

    Feat: Register `PlatformTrendingPlayersController` in `PlatformTrendingModule`
    Signed-off-by: Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>

--- api/src/platform-trending/platform-trending.module.ts
+++ api/src/platform-trending/platform-trending.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { PlatformTrendingPlayersController } from './platform-trending-players.controller';
 import { PlatformTrendingPlayersProjectionService } from './platform-trending-players-projection.service';
 import { PlatformTrendingPlayersReadService } from './platform-trending-players-read.service';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [PlatformTrendingPlayersController],
   providers: [
     PlatformTrendingPlayersProjectionService,
     PlatformTrendingPlayersReadService,
